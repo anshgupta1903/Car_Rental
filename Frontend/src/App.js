6dd31cbc496b0d746e8402eca366c976558cedc1
Abhishek Arora <abhishekarora866@gmail.com>
2025-09-06 17:22:46 +0530

    frontend enhanced

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import LoginSignup from './components/LoginSignup/LoginSignup';
 import Dashboard from './components/Dashboard/Dashboard';
@@ -11,9 +11,11 @@ function App() {
       <Router>
         <div className="App">
           <Routes>
-            <Route path="/" element={<LoginSignup />} />
+            <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<LoginSignup />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            {/* Redirect any unknown paths to the dashboard */}
+            <Route path="*" element={<Navigate to="/dashboard" />} />
           </Routes>
         </div>
       </Router>
